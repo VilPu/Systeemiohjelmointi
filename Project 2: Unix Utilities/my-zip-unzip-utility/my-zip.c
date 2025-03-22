00ffe3569822cbe498bc9f0a8885cb7d56c761e0
VilPu <vili.puro@gmail.com>
2025-03-22 13:07:55 +0200

    Fixed my-zip Added screenshots of tests passing

--- Project 2: Unix Utilities/my-zip-unzip-utility/my-zip.c
+++ Project 2: Unix Utilities/my-zip-unzip-utility/my-zip.c
@@ -26,116 +26,99 @@ void checkMallocChar(char *input)
     return;
 }
 
-// function to read a line, stops the reading at a newline character
-// returns line in case of successful line read, NULL at EOF
-char *readLineCharbyChar(FILE *file)
+// writes a 5 byte entry to stdout that consists of count(int) and c(ASCII char)
+// returns void
+void writeBinary(int count, int c)
 {
-    int c;
-    int index = 0;
-    char *line = (char *)malloc(2);
-    checkMallocChar(line);
-
-    while ((c = getc(file)) != 10) // in ASCII 10 == '\n'
-    {
-        if (c == EOF && strlen(line) == 0)
-        {
-            free(line);
-            return NULL;
-        }
-        if (c == EOF)
-        {
-            line[index] = '\0'; // adding the null character to end the string properly
-            return line;
-        }
-        
-        line = realloc(line, (strlen(line) + sizeof(c)));
-        checkMallocChar(line);
-        line[index++] = (char)c;
-    }
-    line[index] = c;
-    line[index + 1] = '\0'; // adding the null character to end the string properly
-    return line;
+    fwrite(&count, sizeof(int), 1, stdout);
+    fwrite(&c, sizeof(char), 1, stdout);
 }
 
-void compress(char *line)
+// compresses a string with run-length encoding (RLE)
+// returns void
+void compress(char *content)
 {
     int count = 0;
     int c = -1;
-    size_t nitems = 1;
-    for (int i = 0; i < strlen(line); i++)
+    long lineSize = strlen(content);
+
+    for (int i = 0; i < lineSize; i++)
     {
-        // handle 1st char in line
+        // handle 1st char in content
         if (c == -1)
         {
-            c = line[i];
+            c = content[i];
             count++;
+            // edge case where content has length of 1
+            if (i + 1 == lineSize)
+            {
+                writeBinary(count, c);
+            }
             continue;
         }
-        // if char is the same
-        if (c == line[i] && strlen(line) != i + 1)
+        // if char is the same and not last char in content
+        if (c == content[i] && lineSize != i + 1)
         {
             count++;
         }
-        // else when char not the same or at the EOF
+        // else when char not the same or at the last char in content
         else
         {
-            // add current char to count in case of EOF
-            if (strlen(line) == i + 1)
+            // edge case if only 1 line of text and no newline at end of file
+            if (c == content[i])
             {
                 count++;
             }
-            char str[2];
-            sprintf(str, "%c", c);
-            fwrite(&count, sizeof(int), nitems, stdout);
-            fwrite(str, sizeof(char), nitems, stdout);
+            writeBinary(count, c);
+
+            // edge case if the last char in content and the char is a newline '\n'
+            if (strlen(content) == i + 1 && content[i] == 10) // ASCII 10 = '\n'
+            {
+                c = content[i];
+                writeBinary(1, c);
+            }
             count = 1;
-            c = line[i];
+            c = content[i];
         }
     }
-    
 }
 
-// reads a file line by line using the readLineCharbyChar function
-// returns void
-void readFile(FILE *file)
+// reads a file as a whole and saves it to content
+// returns pointer to file(s) content
+char *readFile(FILE *file, char *content)
 {
     checkFile(file);
-    while (feof(file) == 0)
-    {
-        char *line;
-        line = readLineCharbyChar(file);
-        if (line == NULL)
-        {
-            break;
-        }
-        compress(line);
-        
-        if (ferror(stdout))
-        {
-            fprintf(stdout, "Error occurred writing file\n");
-            free(line);
-            break;
-        }
-        free(line);
-    }
-    return;
+    fseek(file, 0, SEEK_END);
+    long size = ftell(file);
+    fseek(file, 0, SEEK_SET);
+
+    content = realloc(content, (strlen(content) + size));
+    char *buffer = malloc(size);
+    checkMallocChar(content);
+    checkMallocChar(buffer);
+    fread(buffer, size, 1, file);
+    strcat(content, buffer);
+    return content;
 }
 
 int main(int argc, char const *argv[])
 {
     FILE *file;
     char *fileName = NULL;
+    char *content = malloc(3);
+    checkMallocChar(content);
 
     // switch-case for different command line arguments
     switch (argc)
     {
     case 1:
+        free(content);
         fprintf(stdout, "wzip: file1 [file2 ...]\n");
         exit(1);
     case 2:
         fileName = (char *)argv[1];
         file = fopen(fileName, "r");
-        readFile(file);
+        content = readFile(file, content);
         fclose(file);
         break;
     default:
@@ -143,10 +126,12 @@ int main(int argc, char const *argv[])
         {
             fileName = (char *)argv[i];
             file = fopen(fileName, "r");
-            readFile(file);
+            content = readFile(file, content);
             fclose(file);
         }
         break;
     }
+    compress(content);
+    free(content);
     exit(0);
 }
