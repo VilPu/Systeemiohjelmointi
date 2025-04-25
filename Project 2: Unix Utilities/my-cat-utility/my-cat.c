d00e277b6b34dd2d95966c6d2b99807ab04feafd
VilPu <vili.puro@gmail.com>
2025-04-25 14:21:24 +0300

    Added reverse-tests screenshot Fixed no newline at eof bug in my-cat

--- Project 2: Unix Utilities/my-cat-utility/my-cat.c
+++ Project 2: Unix Utilities/my-cat-utility/my-cat.c
@@ -39,8 +39,7 @@ char *readLineCharbyChar(FILE *file)
     {
         if (c == EOF)
         {
-            free(line);
-            return NULL;
+            return line;
         }
         line = realloc(line, (strlen(line) + sizeof(c)));
         checkMallocChar(line);
